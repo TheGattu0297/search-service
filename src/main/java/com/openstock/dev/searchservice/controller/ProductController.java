73a473152d2825657f31f2033614d7341735e8de
vishalOS27 <vishal@openstock.co.uk>
2024-04-19 17:25:40 +0100

    Filter APIs and code improvement

--- src/main/java/com/openstock/dev/searchservice/controller/ProductController.java
+++ src/main/java/com/openstock/dev/searchservice/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.openstock.dev.searchservice.controller;
 import com.openstock.dev.searchservice.model.Product;
 import com.openstock.dev.searchservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,18 +12,22 @@ import java.util.List;
 @RequestMapping(value = "/search/product")
 public class ProductController {
 
-    @Autowired
     private ProductService productService;
 
-
-    @PostMapping("/saveAll")
-    public List<Product> addProducts (@RequestBody List<Product> products){
-        return productService.saveProducts(products);
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @PostMapping("/save")
-    public Product addProductById(@RequestBody Product product){
-        return productService.saveProduct(product);
+    public ResponseEntity<String> addProductById(@RequestBody Product product){
+        productService.saveProduct(product);
+        return ResponseEntity.ok("Product inserted successfully!");
+    }
+
+    @PostMapping("/saveAll")
+    public ResponseEntity<String> addProducts (@RequestBody List<Product> products){
+        productService.saveProducts(products);
+        return ResponseEntity.ok("Products inserted successfully!");
     }
 
     @GetMapping("/getAll")
@@ -43,6 +48,51 @@ public class ProductController {
     @GetMapping("/type/{type}")
     public List<Product> getProductsByType(@PathVariable("type") String type) {
         return productService.findByType(type);
+    }
+
+    @GetMapping("/region/{region}")
+    public List<Product> getProductsByRegion(@PathVariable("region") String region) {
+        return productService.findByRegion(region);
+    }
+
+    @GetMapping("/subRegion/{subRegion}")
+    public List<Product> getProductsBySubRegion(@PathVariable("subRegion") String subRegion) {
+        return productService.findBySubRegion(subRegion);
+    }
+
+    @GetMapping("/denomination/{denomination}")
+    public List<Product> getProductsByDenomination(@PathVariable("denomination") String denomination) {
+        return productService.findByDenomination(denomination);
+    }
+
+    @GetMapping("/producer/{producer}")
+    public List<Product> getProductsByProducer(@PathVariable("producer") String producer) {
+        return productService.findByProducer(producer);
+    }
+
+    @GetMapping("/name/{name}")
+    public List<Product> getProductsByName(@PathVariable("name") String name) {
+        return productService.findByName(name);
+    }
+
+    @GetMapping("/variety/{variety}")
+    public List<Product> getProductsByVariety(@PathVariable("variety") String variety) {
+        return productService.findByVariety(variety);
+    }
+
+    @GetMapping("/alcoholPercentage/{alcoholPercentage}")
+    public List<Product> getProductsByAlcoholPercentage(@PathVariable("alcoholPercentage") String alcoholPercentage) {
+        return productService.findByAlcoholPercentage(alcoholPercentage);
+    }
+
+    @GetMapping("/vintage/{vintage}")
+    public List<Product> getProductsByVintage(@PathVariable("vintage") String vintage) {
+        return productService.findByVintage(vintage);
+    }
+
+    @GetMapping("/info/{info}")
+    public List<Product> getProductsByInfo(@PathVariable("info") String info) {
+        return productService.findByInfo(info);
     }
 
     @DeleteMapping("/deleteAll")
--- src/main/java/com/openstock/dev/searchservice/model/Product.java
+++ src/main/java/com/openstock/dev/searchservice/model/Product.java
@@ -7,26 +7,40 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 @Document(indexName = "products")
 public class Product {
 
     @Id
+    @Field(type = FieldType.Text, name = "productID")
     private String productID;
+    @Field(type = FieldType.Text, name = "country")
     private String country;
+    @Field(type = FieldType.Text, name = "type")
     private String type;
+    @Field(type = FieldType.Text, name = "region")
     private String region;
+    @Field(type = FieldType.Text, name = "subRegion")
     private String subRegion;
+    @Field(type = FieldType.Text, name = "denomination")
     private String denomination;
+    @Field(type = FieldType.Text, name = "producer")
     private String producer;
+    @Field(type = FieldType.Text, name = "name")
     private String name;
+    @Field(type = FieldType.Text, name = "variety")
     private String variety;
+    @Field(type = FieldType.Text, name = "alcoholPercentage")
     private String alcoholPercentage;
+    @Field(type = FieldType.Text, name = "vintage")
     private String vintage;
+    @Field(type = FieldType.Text, name = "info")
     private String info;
 }
