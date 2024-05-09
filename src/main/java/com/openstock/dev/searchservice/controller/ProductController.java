4efa0fec486c19d52b2a17949ae1d7c65aa12222
vishalOS27 <vishal@openstock.co.uk>
2024-05-09 10:41:12 +0100

    CORS Config

--- src/main/java/com/openstock/dev/searchservice/controller/ProductController.java
+++ src/main/java/com/openstock/dev/searchservice/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/search/product")
 public class ProductController {
--- src/main/java/com/openstock/dev/searchservice/controller/SearchController.java
+++ src/main/java/com/openstock/dev/searchservice/controller/SearchController.java
@@ -2,13 +2,11 @@ package com.openstock.dev.searchservice.controller;
 
 import com.openstock.dev.searchservice.model.Product;
 import com.openstock.dev.searchservice.service.ElasticSearchService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/search/bar")
 public class SearchController {
