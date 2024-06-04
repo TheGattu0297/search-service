8d293a3f2c3533785c5886327d37b73df02969a8
Ram0222 <devs@openstock.co.uk>
2024-06-04 11:40:00 +0100

    Health

---
+++ src/main/java/com/openstock/dev/searchservice/controller/health/HealthController.java
@@ -0,0 +1,16 @@
+package com.openstock.dev.searchservice.controller.health;
+
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/actuator/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Health Check Success ");
+    }
+}
