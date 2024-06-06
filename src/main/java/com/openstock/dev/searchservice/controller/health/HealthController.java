24226114878df0fdc0477b2fb48def32105e1774
vishalOS27 <vishal@openstock.co.uk>
2024-06-06 16:04:05 +0100

    Health

--- src/main/java/com/openstock/dev/searchservice/controller/health/HealthController.java
+++ src/main/java/com/openstock/dev/searchservice/controller/health/HealthController.java
@@ -3,9 +3,11 @@ package com.openstock.dev.searchservice.controller.health;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HealthController {
 
