5a6e6e987904a7dd71a5d49f0f1d1aa7b4b63893
vishalOS27 <vishal@openstock.co.uk>
2024-07-19 14:02:11 +0100

    CORS Config

--- src/main/java/com/openstock/dev/searchservice/configuration/WebConfig.java
+++ src/main/java/com/openstock/dev/searchservice/configuration/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins("https://www.myopenstock.com") // Allow only this origin; use "*" for allowing all
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
-                .allowedHeaders("Authorization", "Content-Type") // Allowed headers
-                .allowCredentials(true) // Allow credentials
+                .allowedOrigins("*") // Allow only this origin; use "*" for allowing all
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                .allowedHeaders("*") // Allowed headers
+                .allowCredentials(false) // Allow credentials
                 .maxAge(3600); // Max age before a preflight request needs to be done again
     }
 }
